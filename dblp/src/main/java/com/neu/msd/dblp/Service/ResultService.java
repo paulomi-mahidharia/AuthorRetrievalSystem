8c4bd0867ed4d1e9995934f4d60323a0d83c83e2
paulomimahidharia <mahidharia.p@husky.neu.edu>
2017-02-17 18:44:55 -0500

    TEAM3-11 #Created result interface

---
+++ dblp/src/main/java/com/neu/msd/dblp/Service/ResultService.java
@@ -0,0 +1,15 @@
+package com.neu.msd.dblp.Service;
+
+import java.util.List;
+
+import com.neu.msd.dblp.model.Result;
+
+public interface ResultService {
+	
+	public List<Result> filterResult(String criteria);
+	
+	public List<Result> sortResult(String sortBy);
+	
+	public String exportResult(String exportType);
+
+}
--- dblp/src/main/java/com/neu/msd/dblp/model/SearchCriteria.java
+++ dblp/src/main/java/com/neu/msd/dblp/model/SearchCriteria.java
@@ -4,5 +4,6 @@ public class SearchCriteria {
 	
 	String authorName;
 	String title;
-	
+	int papersPublished;
+	String conferenceName;
 }
