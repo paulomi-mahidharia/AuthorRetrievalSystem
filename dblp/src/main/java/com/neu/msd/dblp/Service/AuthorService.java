1b4a95925db482eaf78ff45e907c0e5919ae5270
Abhinav Maurya <abhinavmaurya@ccs.neu.edu>
2017-02-17 18:58:44 -0500

    TEAM3-12 #Element comment in AuthorService

--- dblp/src/main/java/com/neu/msd/dblp/Service/AuthorService.java
+++ dblp/src/main/java/com/neu/msd/dblp/Service/AuthorService.java
@@ -1,6 +1,25 @@
 package com.neu.msd.dblp.Service;
 
+import java.util.List;
+
+import com.neu.msd.dblp.model.Author;
+import com.neu.msd.dblp.model.SearchCriteria;
+
 public interface AuthorService {
 	
-	public String getAuthor(String name);
+	/**
+	 * Searches for author by name
+	 * @param name
+	 * @return Author with matching name
+	 */
+	public Author getAuthor(String name);
+	
+	/**
+	 * search authors based on search criteria
+	 * @param criteria
+	 * @return List of Authors matching the search criteria.
+	 */
+	public List<Author> searchAuthors(SearchCriteria criteria);
+	
+	
 }
