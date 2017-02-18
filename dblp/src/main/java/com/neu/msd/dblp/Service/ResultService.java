43de0c3b2d64a317a9e311e522f854fc746c95c0
Aswin Gopalan <rgaswin@ccs.neu.edu>
2017-02-17 19:05:40 -0500

    TEAM3-12 #Comments for ResultService Interface methods

--- dblp/src/main/java/com/neu/msd/dblp/Service/ResultService.java
+++ dblp/src/main/java/com/neu/msd/dblp/Service/ResultService.java
@@ -11,10 +11,23 @@ import com.neu.msd.dblp.model.Result;
  */
 public interface ResultService {
 	
+	/** A function that filters existing results based on a criteria.
+	 * @param criteria - A string to filter results  
+	 * @return A list of result objects filtered by the criteria.
+	 */
 	public List<Result> filterResult(String criteria);
 	
+	
+	/**
+	 * @param sortBy - sort results by the string provided
+	 * @return - A list of objects sorted by the input string. 
+	 */
 	public List<Result> sortResult(String sortBy);
 	
+	/**
+	 * @param exportType - A string requesting format of document to be exported to. 
+	 * @return - A document in the given format exported from the system to the user's file system.
+	 */
 	public String exportResult(String exportType);
 
 }
