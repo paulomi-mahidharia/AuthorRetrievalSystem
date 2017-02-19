df331724d92b41e3b61595a8d9c6d247920bb129
Aswin Gopalan <rgaswin@ccs.neu.edu>
2017-02-19 18:55:34 -0500

    Created Thesis model and Thesis type Enum

---
+++ dblp/src/main/java/com/neu/msd/dblp/model/Thesis.java
@@ -0,0 +1,14 @@
+package com.neu.msd.dblp.model;
+
+import java.util.Date;
+
+public class Thesis {
+	ThesisType type;
+	String title;
+	String Thesis_Key;
+	Date date;
+	String school;
+	int year;
+	int num_of_pages;
+	String url;
+}
