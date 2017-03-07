14f638b9b05ba9af9c37db0514e1b20faad7806d
paulomimahidharia <mahidharia.p@husky.neu.edu>
2017-03-07 15:33:55 -0500

    TEAM3-13 #Wrote valid and invalid login login tests

--- dblp/src/test/java/com/neu/msd/dblp/UserTest.java
+++ dblp/src/test/java/com/neu/msd/dblp/UserTest.java
@@ -1,7 +1,7 @@
 package com.neu.msd.dblp;
 
 import org.junit.Test;
-
+import org.mockito.Mockito;
 import com.neu.msd.dblp.service.UserService;
 import com.neu.msd.dblp.service.UserServiceImpl;
 
@@ -15,6 +15,43 @@ public class UserTest extends TestCase{
       
       UserService userService = new UserServiceImpl();
       assertFalse(userService.login("", ""));
+   }
+   
+   @Test
+   public void testNullUsernamePassword(){
+      
+      UserService userService = new UserServiceImpl();
+      assertFalse(userService.login(null, null));
+   }
+   
+   @Test
+   public void testEmptyUsernameNullPassword(){
+      
+      UserService userService = new UserServiceImpl();
+      assertFalse(userService.login("", null));
+   }
+   
+   @Test
+   public void testNullUsernameEmptyPassword(){
+      
+      UserService userService = new UserServiceImpl();
+      assertFalse(userService.login(null, ""));
+   }
+   
+   @Test
+   public void testValidUsernamePassword(){
+	  
+      UserService userService = Mockito.mock(UserService.class);
+      Mockito.when(userService.login("paulomi", "paulomi")).thenReturn(true);
+      assertTrue(userService.login("paulomi", "paulomi"));
+   }
+   
+   @Test
+   public void testValidUsernameInvalidPassword(){
+	  
+      UserService userService = Mockito.mock(UserService.class);
+      Mockito.when(userService.login("paulomi", "p")).thenReturn(false);
+      assertFalse(userService.login("paulomi", "p"));
    }
    
 }
