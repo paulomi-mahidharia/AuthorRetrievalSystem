64c93737566a853675245fc9b49f97ce5affb0c7
paulomimahidharia <mahidharia.p@husky.neu.edu>
2017-03-25 19:02:29 -0400

    TEAM3-59 #Started working on Search Page

--- AuthorRetriever/src/main/java/application/Main.java
+++ AuthorRetriever/src/main/java/application/Main.java
@@ -4,13 +4,18 @@ import com.neu.msd.AuthorRetriever.service.UserService;
 import com.neu.msd.AuthorRetriever.service.UserServiceImpl;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -20,6 +25,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -31,8 +37,8 @@ public class Main extends Application {
 			grid.setHgap(10);
 			grid.setVgap(10);
 			grid.setPadding(new Insets(25, 25, 25, 25));
-			Scene scene = new Scene(grid,400,400, Color.BEIGE);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(grid,1000,1000, Color.BEIGE);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			Text scenetitle = new Text("Welcome!");
 			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
@@ -73,15 +79,99 @@ public class Main extends Application {
 	            	
 	            	if(isLoginSuccessful){
 	            		GridPane grid2 = new GridPane();
-	            		grid2.setAlignment(Pos.CENTER);
+	            		grid2.setAlignment(Pos.TOP_LEFT);
 	            		grid2.setHgap(10);
 	            		grid2.setVgap(10);
 	            		grid2.setPadding(new Insets(25, 25, 25, 25));
 	        			
-	            		Scene scene2 = new Scene(grid2,400,400, Color.BEIGE);
+	            		Scene scene2 = new Scene(grid2,1000,1000, Color.BEIGE);
 	            		
-	            		Text scenetitle = new Text("Welcome to Search page!");
-	            		grid2.add(scenetitle, 0, 0, 2, 1);
+	            		Text scenetitle = new Text("SEARCH AUTHORS");
+	            		scenetitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 24));
+	            		scenetitle.setFill(Color.FIREBRICK);
+	            		grid2.add(scenetitle, 0, 0);
+	            		
+	            		CheckBox paperCheck = new CheckBox("Search based on paper information");
+	            		paperCheck.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+	            		grid2.add(paperCheck, 0, 2);
+	            		
+	            		Label numberOfPapers = new Label("Number of minimum Publications:");
+	        			grid2.add(numberOfPapers, 0, 3);
+	        			
+	        			TextField numberOfPapersField = new TextField();
+	        			grid2.add(numberOfPapersField, 1, 3);
+	        			
+	        			Label nameOfConference = new Label("Conference published/not published for:");
+	        			grid2.add(nameOfConference, 0, 4);
+	        			
+	        			ObservableList<String> options = 
+	        				    FXCollections.observableArrayList(
+	        				        "Published",
+	        				        "Not published"
+	        				    );
+        				ComboBox comboBox = new ComboBox(options);
+        				comboBox.setValue("Published");
+        				grid2.add(comboBox, 1, 4);
+        				
+        				Label yearRange = new Label("Year range:");
+	        			grid2.add(yearRange, 0, 5);
+	        			
+	        			ObservableList<String> options1 = 
+	        				    FXCollections.observableArrayList(
+	        				        "between",
+	        				        "before",
+	        				        "after"
+	        				    );
+        				ComboBox comboBox1 = new ComboBox(options1);
+        				comboBox1.setValue("before");
+        				grid2.add(comboBox1, 1, 5);
+        				
+        				final TextField fromYear = new TextField();
+        		        final Label betweenYearLabel = new Label("to"); 
+        		        final TextField toYear = new TextField();
+        		        grid2.add(fromYear, 2, 5);
+        		        System.out.println(comboBox1.getValue());
+        		        
+        		        comboBox1.setOnAction(new EventHandler<ActionEvent>() {
+        		        	 
+        		            @Override
+        		            public void handle(ActionEvent e) {
+        		            	String comboSelection = comboBox1.getSelectionModel().getSelectedItem().toString();
+            		            
+            		            if(comboBox1.getValue().equals("between")){
+            		            	if(!grid2.getChildren().contains(betweenYearLabel)){
+            		            		grid2.add(betweenYearLabel, 3, 5);
+                		        	}
+            		            	if(!grid2.getChildren().contains(toYear)){
+            		            		grid2.add(toYear, 4, 5);
+            		            	}
+                		        }else if(comboBox1.getValue().equals("before")){
+                		        	if(grid2.getChildren().contains(betweenYearLabel)){
+                		        		grid2.getChildren().remove(betweenYearLabel);
+                		        	}
+                		        	if(grid2.getChildren().contains(toYear)){
+                		        		grid2.getChildren().remove(toYear);
+                		        	}
+                		        	if(!grid2.getChildren().contains(fromYear)){
+                    		        	grid2.add(fromYear, 2, 5);
+                		        	}
+                		        	
+                		        }else if(comboBox1.getValue().equals("after")){
+                		        	if(grid2.getChildren().contains(betweenYearLabel)){
+                		        		grid2.getChildren().remove(betweenYearLabel);
+                		        	}
+                		        	if(grid2.getChildren().contains(toYear)){
+                		        		grid2.getChildren().remove(toYear);
+                		        	}
+                		        	if(!grid2.getChildren().contains(fromYear)){
+                    		        	grid2.add(fromYear, 2, 5);
+                		        	}
+                		        }
+        		            }
+        		        });
+        		            
+        		      
+	            		
 	        			primaryStage.setScene(scene2);
 	        			primaryStage.show();
 	            		
@@ -101,5 +191,14 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public GridPane getGridPane(){
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		return grid;
 	}
 }
