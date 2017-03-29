ab1acf70e6d4f416f884ce59252bb00b8de3594b
Abhinav Maurya <abhinavmaurya@ccs.neu.edu>
2017-03-29 18:28:26 -0400

    TEAM3-73 #added Paper Dao

--- AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/dao/SearchPaperDao.java
+++ AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/dao/SearchPaperDao.java
@@ -3,9 +3,10 @@ package com.neu.msd.AuthorRetriever.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.neu.msd.AuthorRetriever.model.AuthorPaper;
 import com.neu.msd.AuthorRetriever.model.Paper;
 
 public interface SearchPaperDao {
 
-	public List<Paper> retrievePapers(String queryString) throws SQLException;
+	public List<AuthorPaper> retrievePapers(String queryString) throws SQLException;
 }
--- AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/dao/SearchPaperDaoImpl.java
+++ AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/dao/SearchPaperDaoImpl.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.neu.msd.AuthorRetriever.model.Author;
+import com.neu.msd.AuthorRetriever.model.AuthorPaper;
 import com.neu.msd.AuthorRetriever.model.Paper;
 import com.neu.msd.AuthorRetriever.service.DatabaseConnection;
 
@@ -15,14 +17,21 @@ public class SearchPaperDaoImpl implements SearchPaperDao {
 	private Connection conn = DatabaseConnection.getConn();
 	
 	@Override
-	public List<Paper> retrievePapers(String queryString) throws SQLException {
+	public List<AuthorPaper> retrievePapers(String queryString) throws SQLException {
 		
 		PreparedStatement stmt = conn.prepareStatement(queryString);
 		ResultSet rs = stmt.executeQuery();
 		
-		List<Paper> papers = new ArrayList<Paper>();
+		List<AuthorPaper> papers = new ArrayList<AuthorPaper>();
 		while(rs.next()){
-			//populate papers list
+			AuthorPaper p = new AuthorPaper();
+			p.setPaperId(Integer.parseInt(rs.getString(1)));
+			p.setPaperTitle(rs.getString(3));
+			p.setYear(Integer.parseInt(rs.getString(5)));
+			p.setConfName(rs.getString(8));
+			p.setUrl(rs.getString(7));
+			
+			papers.add(p);
 		}
 		return papers;
 	}
