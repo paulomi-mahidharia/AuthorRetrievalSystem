0daa2e9c3842993ecd6064bf6c77b0c602c0f999
Rushikesh Badami <rbadami@ccs.neu.edu>
2017-04-06 00:58:27 -0400

    Implemented login api for login

--- AuthorRetriever/src/test/java/com/neu/msd/AuthorRetriever/dao/TestUserDao.java
+++ AuthorRetriever/src/test/java/com/neu/msd/AuthorRetriever/dao/TestUserDao.java
@@ -11,8 +11,8 @@ public class TestUserDao extends TestCase {
 		UserDao userDao = new UserDaoImpl();
 		String username= "a";
 		String password = "b";
-		
-		assertTrue(userDao.login(username, password));
+		String queryString="select password from UserCredentials where username=?";
+		assertTrue(userDao.login(username, password,queryString));
 	}
 	
 	@Test
@@ -20,8 +20,8 @@ public class TestUserDao extends TestCase {
 		UserDao userDao = new UserDaoImpl();
 		String username= "a";
 		String password = "";
-		
-		assertFalse(userDao.login(username, password));
+		String queryString="select password from UserCredentials where username=?";
+		assertFalse(userDao.login(username, password,queryString));
 	}
 	
 	// more cases
