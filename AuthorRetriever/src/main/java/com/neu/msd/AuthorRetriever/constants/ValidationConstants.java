70d6bcd1b9393a9bc723fe9e927524ba76949430
paulomimahidharia <mahidharia.p@husky.neu.edu>
2017-04-11 00:38:04 -0400

    Finished Scene Stack modeling

--- AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/constants/ValidationConstants.java
+++ AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/constants/ValidationConstants.java
@@ -19,4 +19,5 @@ public class ValidationConstants {
 	public static final String INVALID_KEYWORD = "Please enter a valid keyword or title";
 	
 	public static final String SQL_FAILURE = "There was problem while retrieving authors. Please try again later.";
+	public static final String NO_AUTHORS_FOUND = "No authors found for given search criteria";
 }
