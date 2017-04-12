27de57607900e7bff0b32eda32b8f0b4329b87ef
Abhinav Maurya <abhinavmaurya@ccs.neu.edu>
2017-04-12 09:50:28 -0400

    Added service to retrieve conference. Added function in AuhorServiceImpl.

--- AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/service/AuthorInfoServiceImpl.java
+++ AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/service/AuthorInfoServiceImpl.java
@@ -6,15 +6,19 @@ import java.util.List;
 
 import com.neu.msd.AuthorRetriever.dao.SearchAuthorDao;
 import com.neu.msd.AuthorRetriever.dao.SearchAuthorDaoImpl;
+import com.neu.msd.AuthorRetriever.dao.SearchConferenceDao;
+import com.neu.msd.AuthorRetriever.dao.SearchConferenceDaoImpl;
 import com.neu.msd.AuthorRetriever.dao.SearchPaperDao;
 import com.neu.msd.AuthorRetriever.dao.SearchPaperDaoImpl;
 import com.neu.msd.AuthorRetriever.model.Author;
 import com.neu.msd.AuthorRetriever.model.AuthorPaper;
+import com.neu.msd.AuthorRetriever.model.Conference;
 
 public class AuthorInfoServiceImpl implements AuthorInfoService {
 
 	private SearchPaperDao searchPaperDao = new SearchPaperDaoImpl();
 	private SearchAuthorDao searchAuthorDao = new SearchAuthorDaoImpl();
+	private SearchConferenceDao searchConfDao = new SearchConferenceDaoImpl();
 	
 	@Override
 	public Author getAuthorProfile(int authorId) {
@@ -39,6 +43,17 @@ public class AuthorInfoServiceImpl implements AuthorInfoService {
 		authorPapers = searchPaperDao.retrievePapers(queryString);
 		
 		return authorPapers;
+	}
+
+	@Override
+	public List<Conference> getAuthorConferenceServed(int authorId) throws SQLException {
+		
+		List<Conference> authorConfServed = new ArrayList<Conference>();
+		
+		String queryString = "SELECT * FROM conference WHERE id IN (SELECT confid FROM conference_editor_mapping where editorid = "+authorId+")";
+		
+		authorConfServed = searchConfDao.retrieveConference(queryString);
+		return authorConfServed;
 	}
 
 }
