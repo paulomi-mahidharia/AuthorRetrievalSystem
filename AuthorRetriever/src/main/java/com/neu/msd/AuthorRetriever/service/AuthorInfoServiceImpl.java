7a6fc3f0085daa19a5263a28294a43acb451c815
Aswin Gopalan <rgaswin@ccs.neu.edu>
2017-04-13 14:49:06 -0400

    Fix for conf_id column

--- AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/service/AuthorInfoServiceImpl.java
+++ AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/service/AuthorInfoServiceImpl.java
@@ -50,7 +50,7 @@ public class AuthorInfoServiceImpl implements AuthorInfoService {
 		
 		List<Conference> authorConfServed = new ArrayList<Conference>();
 		
-		String queryString = "SELECT * FROM conference WHERE conf_id IN (SELECT confid FROM conference_editor_mapping where editorid = "+authorId+")";
+		String queryString = "SELECT * FROM conference WHERE ID IN (SELECT confid FROM conference_editor_mapping where editorid = "+authorId+")";
 
 		
 		authorConfServed = searchConfDao.retrieveConference(queryString);
