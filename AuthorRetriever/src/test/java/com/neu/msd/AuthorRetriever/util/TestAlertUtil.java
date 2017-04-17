6a4b30dad86ee0059323e36524d08067af288b6d
Abhinav Maurya <abhinavmaurya@ccs.neu.edu>
2017-04-17 01:17:53 -0400

    Added dummy cases to test jenkins

--- AuthorRetriever/src/test/java/com/neu/msd/AuthorRetriever/util/TestAlertUtil.java
+++ AuthorRetriever/src/test/java/com/neu/msd/AuthorRetriever/util/TestAlertUtil.java
@@ -1,7 +1,13 @@
 package com.neu.msd.AuthorRetriever.util;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class TestAlertUtil extends TestCase {
 
+	@Test
+	public void testAlertUtil(){
+		assertTrue(true);
+	}
 }
--- AuthorRetriever/src/test/java/com/neu/msd/AuthorRetriever/util/TestGroupByUtil.java
+++ AuthorRetriever/src/test/java/com/neu/msd/AuthorRetriever/util/TestGroupByUtil.java
@@ -1,7 +1,13 @@
 package com.neu.msd.AuthorRetriever.util;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class TestGroupByUtil extends TestCase {
 
+	@Test
+	public void testGroupByUtil(){
+		assertTrue(true);
+	}
 }
