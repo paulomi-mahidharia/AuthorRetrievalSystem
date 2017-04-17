e4df9e88d661a566449c7e1c72ecb6f1311d2529
paulomimahidharia <mahidharia.p@husky.neu.edu>
2017-04-17 14:18:48 -0400

    TEAM3-99 #Fixed password pattern error

--- AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/constants/ValidationConstants.java
+++ AuthorRetriever/src/main/java/com/neu/msd/AuthorRetriever/constants/ValidationConstants.java
@@ -38,6 +38,6 @@ public class ValidationConstants {
 	public static final String EMPTY_AUTHOR = "Author name cannot be empty or null";
 	public static final String INVALID_AUTHOR = "Invalid author name";
 	public static final String VALID_AUHTOR = "Valid author name";
-	public	 static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+	public static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
 
 }
